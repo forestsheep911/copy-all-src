614e4e44f38f0e24544f335816b9697b7c7a3b76
徐冰 <bxu@cybozu.onmicrosoft.com>
2024-10-10 10:38:53 +0800

    build asset

--- src/build.py
+++ src/build.py
@@ -1,7 +1,7 @@
 import os
 
 def build():
-    os.system("pyinstaller --onefile src/main.py")
+    os.system("pyinstaller --onefile --distpath dist src/main.py")
 
 if __name__ == "__main__":
     build()
