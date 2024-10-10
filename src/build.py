e351d807d59ac5ac75abcb196f49b53beee623e7
forestsheep911 <bxu@cybozu.onmicrosoft.com>
2024-10-10 15:38:05 +0800

    解决文件名不正确

--- src/build.py
+++ src/build.py
@@ -7,10 +7,15 @@ def build():
     os.system("pyinstaller --onefile --distpath dist src/main.py")
     if os.name == "nt":
         shutil.move("dist/main.exe", "dist/cpsrc.exe")
-    else:
-        shutil.move("dist/main", "dist/cpsrc")
-        os.chmod("dist/cpsrc", stat.S_IRWXU)
-
-
-if __name__ == "__main__":
-    build()
+        shutil.move("dist/main.exe", "dist/cpsrc-win.exe")
+    elif os.name == "posix":
+        if "darwin" in os.uname().sysname.lower():
+            shutil.move("dist/main", "dist/cpsrc")
+            os.chmod("dist/cpsrc", stat.S_IRWXU)
+            shutil.move("dist/main", "dist/cpsrc-mac")
+            os.chmod("dist/cpsrc-mac", stat.S_IRWXU)
+        else:
+            shutil.move("dist/main", "dist/cpsrc")
+            os.chmod("dist/cpsrc", stat.S_IRWXU)
+            shutil.move("dist/main", "dist/cpsrc-linux")
+            os.chmod("dist/cpsrc-linux", stat.S_IRWXU)
